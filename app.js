f0267e63a88520944f4b2a850ba3d073bca4119d
mcdchristian <mcdchristian4@gmail.com>
2025-07-17 19:14:11 +0200

    first route and express endpoint

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+	res.send('Hello express 2v');
+});
+app.get('/api/pokemons/:id', (req, res) => {
+	const id = req.params.id;
+	res.send(`vous avez demandé le pokemon avec avec l'id ${id}`);
+});
+app.listen(port, () => console.log(`server is running on port ${port}`));
