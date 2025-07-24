2a52255faa02c66059213374ac3acc3d64a616c3
mcdchristian <mcdchristian4@gmail.com>
2025-07-24 17:55:44 +0200

    Ajout de l'API Pokémon avec Sequelize, déplacement de mock-pokemon.js vers src/db/, suppression de helper.js, retrait de node_modules/.package-lock.json, mise à jour des dépendances, et ajout de .gitignore et src/

---
+++ src/routes/findAllpokemons.js
@@ -0,0 +1,16 @@
+import { Pokemon } from '../db/sequelize.js';
+
+export default (app) => {
+	app.get('/api/pokemons', (req, res) => {
+		Pokemon.findAll()
+			.then((pokemons) => {
+				const message = 'La liste des pokémons a bien été récupérée.';
+				res.json({ message, data: pokemons });
+			})
+			.catch((error) => {
+				const message =
+					"La liste des pokémons n'a pas pu être récupérée. Réessayez dans quelques instants.";
+				res.status(500).json({ message, data: error });
+			});
+	});
+};
