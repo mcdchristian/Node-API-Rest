b7b76a8b0d2eaa9570bc06ed5c6c7485edb25baa
mcdchristian <mcdchristian4@gmail.com>
2025-07-28 19:06:41 +0200

    Restauration complète des fichiers src/routes depuis le commit 2a52255

--- src/routes/findPokemonByPk.js
+++ src/routes/findPokemonByPk.js
@@ -2,20 +2,9 @@ import { Pokemon } from '../db/sequelize.js';
 
 export default (app) => {
 	app.get('/api/pokemons/:id', (req, res) => {
-		Pokemon.findByPk(req.params.id)
-			.then((pokemon) => {
-				if (pokemon === null) {
-					const message =
-						"le pokemon demandé n'existe pas. Réessayez avec un autre identifiant";
-					return res.status(404).json({ message });
-				}
-				const message = 'Un pokémon a bien été trouvé.';
-				res.json({ message, data: pokemon });
-			})
-			.catch((error) => {
-				const message =
-					"le pokemon n'a pas pu être recupéré. Résessayez dans quelques instants";
-				res.status(500).json({ message, data: error });
-			});
+		Pokemon.findByPk(req.params.id).then((pokemon) => {
+			const message = 'Un pokémon a bien été trouvé.';
+			res.json({ message, data: pokemon });
+		});
 	});
 };
