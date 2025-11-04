be0d670adb7d3d7b72da800ffe9df8095fefb078
mcdchristian <mcdchristian4@gmail.com>
2025-11-04 19:26:48 +0200

    export default sequelize example

--- src/routes/login.js
+++ src/routes/login.js
@@ -34,3 +34,20 @@ export default (app) => {
 			});
 	});
 };
+
+// export default (sequelize, DataTypes) => {
+// 	return sequelize.define('User', {
+// 		id: {
+// 			type: DataTypes.INTEGER,
+// 			primaryKey: true,
+// 			autoIncrement: true,
+// 		},
+// 		username: {
+// 			type: DataTypes.STRING,
+// 			unique: { msg: 'le nom est déjà pris' },
+// 		},
+// 		password: {
+// 			type: DataTypes.STRING,
+// 		},
+// 	});
+// };
