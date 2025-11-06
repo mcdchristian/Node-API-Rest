55eee8c42ea3b0a052d3b33fb82c69ef0e489247
mcdchristian <mcdchristian4@gmail.com>
2025-11-06 16:36:48 +0200

    update file user.js

--- src/models/user.js
+++ src/models/user.js
@@ -14,3 +14,18 @@ export default (sequelize, DataTypes) => {
 		},
 	});
 };
+
+//filter method example
+
+// filter method exmple
+
+// const filteredPrices = prices.filter((price) => price > 6);
+// console.log(filteredPrices);
+
+// const fruits = ['pomme', 'banane', 'poire', 'pomme'];
+// const pommes = fruits.filter((fruit, index, arr) => {
+// 	console.log(`Index: ${index}, Tableau: ${arr}`);
+// 	return fruit === 'pomme';
+// });
+
+// console.log(pommes);
