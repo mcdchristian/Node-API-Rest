d9ea5e58471bf9a8f66a15e66efc51c9efc08dc5
mcdchristian <mcdchristian4@gmail.com>
2025-12-01 12:46:52 +0200

    Examples of arrow functions refreshing js basics

--- src/auth/private_key.js
+++ src/auth/private_key.js
@@ -1,1 +1,12 @@
 export default 'CUSTOM_PRIVATE_KEY';
+
+// Examples of arrow functions refreshing js basics
+
+// const add = (a, b) => a + b;
+// console.log(add(1, 2));
+
+// const addOne = (a) => a + 1;
+// console.log(addOne(1));
+
+// const addRandom = () => 1 + 2;
+// console.log(addRandom());
