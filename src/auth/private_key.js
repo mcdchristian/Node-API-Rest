f350fc18d256b4251f638bc855816641ed4bb8cc
mcdchristian <mcdchristian4@gmail.com>
2026-01-10 18:49:17 +0200

    private_key js

--- src/auth/private_key.js
+++ src/auth/private_key.js
@@ -1,25 +1,6 @@
 export default 'CUSTOM_PRIVATE_KEY';
 
-// Examples of arrow functions refreshing js basics
-
-// const add = (a, b) => a + b;
-// console.log(add(1, 2));
-
-// const addOne = (a) => a + 1;
-// console.log(addOne(1));
-
-// const addRandom = () => 1 + 2;
-// console.log(addRandom());
-
-// Example of object methods and this keyword basics js
-
-// const person = {
-// 	name: "Del'or",
-// 	age: 23,
-// 	greet: function () {
-// 		console.log('Hi, I am ' + this.name);
-// 	},
-// };
-
-// // console.log(person);
-// person.greet();
+// const router = express.Router();
+// router.get('/', (req, res, next) => {
+// 	res.send('<h1>Hello from Express!</h1>');
+// });
